c39c75f0fcc12c4367ea5da080ca20977c228a0f
blackngold <jesusgabisanchez@gmail.com>
2023-06-02 11:51:44 -0400

    Added time stamp to logger

--- AppLogger.py
+++ AppLogger.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from datetime import datetime
 
 # Logging levels
 # logging.debug - diagnosing problems
@@ -8,24 +9,30 @@ import os
 # .error        - software unable to perform a function
 # .critical     - software really in problem now
 
+def getTime():
+    timestamp = datetime.now()
+    timestamp = timestamp.strftime("%m-%d-%Y %H:%M:%S")
+    return timestamp
+
 def init():
     path_to_log = os.getcwd()
     logging.basicConfig(filename=(path_to_log + '\\log\\log.txt'), encoding='utf-8',
             level=logging.DEBUG)
+    logging.info(getTime() + " | Logger initiated.")
     
 def log(msg):
-    logging.warn(msg)
+    logging.warn(getTime() + " | " + msg)
 
 def log_debug(msg):
-    logging.debug(msg)
+    logging.debug(getTime() + " | " + msg)
 
 def log_warning(msg):
-    logging.warning(msg)
+    logging.warning(getTime() + " | " + msg)
 
 def log_error(msg):
-    logging.error(msg)
+    logging.error(getTime() + " | " + msg)
 
 def log_critical(msg):
-    logging.critical(msg)
+    logging.critical(getTime() + " | " + msg)
 
 # TODO - create functions to log SQL queries
